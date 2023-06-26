659bbe73b1a0694b54c92c00da036f94156ca573
Ansar Ismagilov <ansar89kab@bk.ru>
2023-06-26 17:04:32 +0800

    Enhancing the Hello World REST API to return a Bean

--- src/main/java/com/kabgig/rest/websevices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/kabgig/rest/websevices/restfulwebservices/helloworld/HelloWorldController.java
@@ -10,4 +10,9 @@ public class HelloWorldController {
     public String helloWorld(){
         return "HelloWorld";
     }
+
+    @GetMapping (path="/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("HelloWorld");
+    }
 }
