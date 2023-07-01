0396e7caefb640a55d4c6801f4f37f4a015fd3e9
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-01 15:05:49 +0800

    Versioning REST API - Request Param, Header and Content Negotiation

--- src/main/java/com/kabgig/rest/websevices/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/kabgig/rest/websevices/restfulwebservices/versioning/VersioningPersonController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersioningPersonController {
 
+    //URI versioning
     @GetMapping("/v1/person")
     public PersonV1 GetFirstVersionOfPerson(){
         return new PersonV1("Bob Charlie");
@@ -15,4 +16,39 @@ public class VersioningPersonController {
     public PersonV2 GetSecondVersionOfPerson(){
         return new PersonV2(new Name("Bob","Charlie"));
     }
+
+    //Request Parameter versioning
+    @GetMapping(path="/person", params = "version=1")
+    public PersonV1 GetFirstVersionOfPersonRequestParameter(){
+        return new PersonV1("Bob Charlie V1");
+    }
+
+    @GetMapping(path="/person", params = "version=2")
+    public PersonV2 GetSecondVersionOfPersonRequestParameter(){
+        return new PersonV2(new Name("Bob","Charlie V2"));
+    }
+
+    //Custom header versioning
+    @GetMapping(path="/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 GetFirstVersionOfPersonRequestHeader(){
+        return new PersonV1("Bob Charlie V1 - request header");
+    }
+
+    @GetMapping(path="/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 GetSecondVersionOfPersonRequestHeader(){
+        return new PersonV2(new Name("Bob","Charlie V2"));
+    }
+
+    //Media type versioning
+    @GetMapping(path="/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 GetFirstVersionOfPersonAcceptHeader(){
+        return new PersonV1("Bob Charlie V1 - request header");
+    }
+
+    @GetMapping(path="/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 GetSecondVersionOfPersonAcceptHeader(){
+        return new PersonV2(new Name("Bob","Charlie V2"));
+    }
+
+
 }
