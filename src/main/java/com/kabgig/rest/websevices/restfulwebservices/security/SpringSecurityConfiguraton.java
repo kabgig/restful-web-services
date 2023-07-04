67719c67789349cbcdbd6ffabb6d72bd90de589f
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-04 17:10:02 +0800

    Enhancing Spring Security Configuration for Basic Authentication

---
+++ src/main/java/com/kabgig/rest/websevices/restfulwebservices/security/SpringSecurityConfiguraton.java
@@ -0,0 +1,19 @@
+package com.kabgig.rest.websevices.restfulwebservices.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguraton {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+}
